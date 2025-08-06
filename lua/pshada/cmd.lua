454e4f42f4577b6639ad0181a2d0662699f61e38
mart-mihkel <mart.mihkel.aun@gmail.com>
2025-08-06 19:34:25 +0300

    fix: correct cmd group and read event

--- lua/pshada/cmd.lua
+++ lua/pshada/cmd.lua
@@ -2,17 +2,21 @@ local utils = require("pshada.utils")
 
 local M = {}
 
+local grp = vim.api.nvim_create_augroup("pshada-group", { clear = true })
+
 ---@param opts Opts
 function M.setup(opts)
-    vim.api.nvim_create_autocmd({ "UIEnter", "DirChanged" }, {
-        group = vim.api.nvim_create_augroup("pshada-read", {}),
+    vim.api.nvim_create_autocmd({ "VimEnter", "DirChanged" }, {
+        group = grp,
+        desc = "Read per-project shada",
         callback = function()
             utils.read_pshada(opts)
         end,
     })
 
     vim.api.nvim_create_autocmd({ "VimLeavePre", "DirChangedPre" }, {
-        group = vim.api.nvim_create_augroup("pshada-write", {}),
+        group = grp,
+        desc = "Write per-project shada",
         callback = function()
             utils.write_pshada(opts)
         end,
